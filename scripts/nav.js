e18c892cb6eaac3ce5cc8091c615737969d1cfe0
soumil <rahulrc3312a@gmail.com>
2022-02-04 19:12:11 +0530

    optimization

--- scripts/nav.js
+++ scripts/nav.js
@@ -59,7 +59,7 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `-${navHeight}px`,
+  rootMargin: `-${+navHeight + 15}px`,
 });
 
 headerObserver.observe(sectionHero);
